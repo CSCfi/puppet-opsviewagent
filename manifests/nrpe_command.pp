f413bfce26a666d7d81fb0d87d85dde18367836e
Kalle Happonen <kalle.happonen@csc.fi>
2014-06-19 13:02:08 +0300

    Added sudo support to nagios checks

--- manifests/nrpe_command.pp
+++ manifests/nrpe_command.pp
@@ -1,14 +1,30 @@
 define opsviewagent::nrpe_command(
   $script_name,
   $script_arguments,
+  $use_sudo = false,
 ) {
   File[$name] ~> Service['opsview-agent']
 
-  file { $name:
-    path    => "${opsviewagent::nrpe_local_configs_path}/${name}.cfg",
-    content => "command[${name}]=${opsviewagent::nrpe_local_script_path}/${script_name} ${script_arguments}\n",
-    owner   => 'nagios',
-    group   => 'nagios',
-    mode    => 600,
+  if $sudo == false {
+    file { $name:
+      path    => "${opsviewagent::nrpe_local_configs_path}/${name}.cfg",
+      content => "command[${name}]=${opsviewagent::nrpe_local_script_path}/${script_name} ${script_arguments}\n",
+      owner   => 'nagios',
+      group   => 'nagios',
+      mode    => 600,
+    }
+  }
+  else {
+    file { $name:
+      path    => "${opsviewagent::nrpe_local_configs_path}/${name}.cfg",
+      content => "command[${name}]=sudo ${opsviewagent::nrpe_local_script_path}/${script_name} ${script_arguments}\n",
+      owner   => 'nagios',
+      group   => 'nagios',
+      mode    => 600,
+    } 
+
+  sudo::conf { '$name':
+      content  => "nagios ALL=(ALL) NOPASSWD: ${opsviewagent::nrpe_local_script_path}/${script_name}",
+    }
   }
 }
