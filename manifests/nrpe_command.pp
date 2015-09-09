2e953f46736df582cd323933a896a5cb852053ee
Risto Laurikainen <risto.laurikainen@csc.fi>
2015-09-09 14:21:50 +0300

    Fix nrpe_command sudo handling
    A previous commit accidentally dropped "sudo" from sudo commands and
    introduced a change in how sudoers is configured. Add back the sudo call
    and make handling of sudoers behave the same way as before.

--- manifests/nrpe_command.pp
+++ manifests/nrpe_command.pp
@@ -7,15 +7,15 @@ define opsviewagent::nrpe_command(
 
   # script is defined with absolute path
   if $script_name =~ /^\/.*/ {
-    $command = "${script_name} ${script_arguments}\n"
+    $command = "${script_name}"
   } else {
-    $command = "${opsviewagent::nrpe_local_script_path}/${script_name} ${script_arguments}\n"
+    $command = "${opsviewagent::nrpe_local_script_path}/${script_name}"
   }
 
   if $use_sudo == false {
     file { $name:
       path    => "${opsviewagent::nrpe_local_configs_path}/${name}.cfg",
-      content => "command[${name}]=${command}",
+      content => "command[${name}]=${command} ${script_arguments}\n",
       owner   => 'nagios',
       group   => 'nagios',
       mode    => '0600',
@@ -24,14 +24,14 @@ define opsviewagent::nrpe_command(
   else {
     file { $name:
       path    => "${opsviewagent::nrpe_local_configs_path}/${name}.cfg",
-      content => "command[${name}]=${command}",
+      content => "command[${name}]=sudo ${command} ${script_arguments}\n",
       owner   => 'nagios',
       group   => 'nagios',
       mode    => '0600',
     }
 
     sudo::conf { $name:
-      content  => "Defaults !requiretty\nnagios ALL=(ALL) NOPASSWD: ${command}",
+      content  => "Defaults !requiretty\nnagios ALL=(ALL) NOPASSWD: ${command}\n",
     }
   }
 }
