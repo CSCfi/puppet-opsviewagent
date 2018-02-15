9460ae33dfaa9f3061ea204ccae226ad8412fe31
Oscar Kraemer <oscar.kraemer@csc.fi>
2018-02-15 09:13:45 +0200

    Updated variable name for flavor count (#29)

--- files/nrpe/check_openstack_usage_stats.py
+++ files/nrpe/check_openstack_usage_stats.py
@@ -86,7 +86,7 @@ def get_per_flavor_active_vm_count(nova):
        name = nova.flavors.get(flavor)
      except NovaNotFound:
        continue
-     flavor_name_dict["num_vms_" + str(name.name).replace(".", '_')] = flavor_id_dict[flavor]
+     flavor_name_dict["num_vms_by_flavors." + str(name.name).replace(".", '_')] = flavor_id_dict[flavor]
   return flavor_name_dict
 
 
