d664952e435853a8f61602e926593c5bc9fc82e8
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-03-12 07:33:12 +0200

    Error checking
    Check that information was parsed from log.

--- files/nrpe/check_exabgp_med_announce.sh
+++ files/nrpe/check_exabgp_med_announce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Check with which Multi-Exit Discriminator value each Service IP is announced
+# Check with which Multi-Exit Discriminator value Service IPs are announced
 # from ExaBGP, based on a log file.
 
 set -e
@@ -10,6 +10,7 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
+# Opportunism.
 TAIL_LINES=1000
 
 usage ()
@@ -17,7 +18,7 @@ usage ()
     echo "Usage: $0 [OPTIONS]"
     echo " -h               Get help"
     echo " -f               Log file to inspect"
-    echo " -a <address>     Address whose MED to check"
+    # echo " -a <address>     Address whose MED to check"
 }
 
 if (($# == 0)); then
@@ -35,9 +36,9 @@ do
         f)
             LOG_FILE_NAME=$OPTARG
             ;;
-        a)
-            CHECK_ADDRESS=$OPTARG
-            ;;
+        # a)
+        #     CHECK_ADDRESS=$OPTARG
+        #     ;;
         \?)
             usage
             exit 1
@@ -59,15 +60,26 @@ if [ "x" == "x$LOG_FILE_NAME" ]; then
   exit ${STATE_WARNING}
 fi
 
-if [ "x" == "x$MULTI_ADDRESS" ]; then
-  echo "option -a is required"
-  exit ${STATE_WARNING}
+# if [ "x" == "x$MULTI_ADDRESS" ]; then
+#   echo "option -a is required"
+#   exit ${STATE_WARNING}
+# fi
+
+LATEST_MED_STATUS=$(tail -${TAIL_LINES} ${LOG_FILE_NAME} | perl -n -e'/(\S+)\/32.*med\s(\d+)\s/ && print "$1 $2\n"' | tail -1)
+SERVICE_IP=echo "$LATEST_MED_STATUS" | awk '{print $1}'
+MED_VALUE=echo "$LATEST_MED_STATUS" | awk '{print $2}'
+COMMUNITY_VALUE=echo "$LATEST_MED_STATUS" | awk '{print $3}'
+
+if [ -z "${SERVICE_IP}"]; then
+  echo "No Service IP information found!"
+  exit ${STATE_CRITICAL}
 fi
 
-LAST_MED_STATUS=$(tail -${TAIL_LINES} /var/log/messages | perl -n -e'/(${CHECK_ADDRESS})\/32.*med\s(\d+)\s/ && print "$1 $2\n"' | tail -1)
-MED_VALUE=echo "$LAST_MED_STATUS" | awk '{print $1}'
-COMMUNITY_VALUE=echo "$LAST_MED_STATUS" | awk '{print $2}'
+if [ -z "${MED_VALUE}"]; then
+  echo "No MED information found!"
+  exit ${STATE_CRITICAL}
+fi
 
-#echo "OK | med=${MED_VALUE};; community=${COMMUNITY_VALUE}"
-echo "OK | med=${MED_VALUE};;"
+#echo "OK | ${SERVICE_IP}_MED=${MED_VALUE};; ${SERVICE_IP}_CTY=${COMMUNITY_VALUE};;"
+echo "OK | ${SERVICE_IP}_MED=${MED_VALUE};;"
 exit ${STATE_OK}
