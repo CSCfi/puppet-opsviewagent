58f89ca00d0f73dd9decbda8c00003ff6c354fbb
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-03-12 15:00:07 +0200

    ExaBGP checks (#30)
    Add two checks:
    
    check_exabgp_med_announce.sh monitors the announced med level of each Service IP
    check_interface_addresses.sh monitors that dummy IPs carry proper Service IPs.

---
+++ files/nrpe/check_exabgp_med_announce.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Check with which Multi-Exit Discriminator value Service IPs are announced
+# from ExaBGP, based on a log file.
+
+set -e
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+# Opportunism
+TAIL_LINES=1000
+
+usage ()
+{
+    echo "Usage: $0 [OPTIONS]"
+    echo " -h               Get help"
+    echo " -f               Log file to inspect"
+}
+
+if (($# == 0)); then
+  usage
+  exit 1
+fi
+
+while getopts ':f:a:h' OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        f)
+            LOG_FILE_NAME=$OPTARG
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+        :)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [ "x" == "x$LOG_FILE_NAME" ]; then
+  echo "option -f is required"
+  exit ${STATE_WARNING}
+fi
+
+RETURN_METRICS=""
+MED_STATUSES_RAW=$(sudo /bin/tail -${TAIL_LINES} ${LOG_FILE_NAME}| perl -n -e'/(\S+)\/32.*med\s(\d+)\s/ && print "$1 $2\n"' | tail -10 | sort | uniq)
+                                                                                                                  #^^^^^^^^^^ more opportunism
+if [ -z "${MED_STATUSES_RAW// }" ]; then
+  echo "Did not get any data when trying to read ${LOG_FILE_NAME}."
+  exit ${STATE_CRITICAL}
+fi
+
+while read -r line; do
+  SERVICE_IP=$(echo "$line"|awk '{print $1}')
+  MED_VALUE=$(echo "$line"|awk '{print $2}')
+  if [ -z "${SERVICE_IP}" ]; then
+    echo "No Service IP information found!"
+    exit ${STATE_CRITICAL}
+  fi
+  if [ -z "${MED_VALUE}" ]; then
+    echo "No MED information found!"
+    exit ${STATE_WARNING}
+  fi
+  SERVICE_IP_NODOTS="$(echo ${SERVICE_IP}|sed 's/\./_/g')"
+  RETURN_METRICS+="${SERVICE_IP_NODOTS}_MED=${MED_VALUE} "
+done <<< "$MED_STATUSES_RAW"
+
+if [ -z "${RETURN_METRICS}" ]; then
+  echo "No metrics parsed from ${LOG_FILE_NAME}."
+  exit ${STATE_CRITICAL}
+fi
+
+echo "OK | ${RETURN_METRICS}"
+exit ${STATE_OK}
---
+++ files/nrpe/check_interface_addresses.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# Check that given interface is UP and has given address.
+
+#set -e
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+usage ()
+{
+    echo "Usage: $0 [OPTIONS]"
+    echo " -h               Get help"
+    echo " -i <interface>   Interface to inspect"
+    echo " -a <address>     Address whose presence to check. Can be repeated."
+}
+
+if (($# == 0)); then
+  usage
+  exit 1
+fi
+
+while getopts ':i:a:h' OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        i)
+            IF_NAME=$OPTARG
+            ;;
+        a)
+            MULTI_ADDRESS+=("$OPTARG")
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+        :)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [ "x" == "x$IF_NAME" ]; then
+  echo "option -i is required"
+  exit ${STATE_WARNING}
+fi
+
+if [ "x" == "x$MULTI_ADDRESS" ]; then
+  echo "option -a is required"
+  exit ${STATE_WARNING}
+fi
+
+IF_HAS_UP=$(ip -4 -o link show ${IF_NAME}|grep -Po "<.*,UP,.*>")
+if [ $? -ne 0 ]; then
+  echo "Interface ${IF_NAME} is not up."
+  exit ${STATE_CRITICAL}
+else
+  echo "Interface ${IF_NAME} is up."
+fi
+
+for IF_CHECK_ADDR in "${MULTI_ADDRESS[@]}"; do
+  IF_HAS_IP=$(ip -4 -o addr show "${IF_NAME}"|grep -Po "${IF_CHECK_ADDR}")
+  if [ $? -ne 0 ]; then
+    echo "Address ${IF_CHECK_ADDR} missing from interface ${IF_NAME}."
+    exit ${STATE_WARNING}
+  else
+    echo "Address ${IF_CHECK_ADDR} present on interface ${IF_NAME}."
+  fi
+done
+
+echo "OK | rc=${STATE_OK}"
+exit ${STATE_OK}
