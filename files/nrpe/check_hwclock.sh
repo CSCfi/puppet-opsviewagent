b43d3c26f6cb8aed95ac63034ef5957ffed40c0f
Khabir Uddin <khuddin@csc.fi>
2019-09-25 17:17:12 +0300

    Some more test added in script to keep it with nagios notification code

--- files/nrpe/check_hwclock.sh
+++ files/nrpe/check_hwclock.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-# This script will check hwclock of the system.
-# Then compare hwclock with system time. if there
-# is more then 5 minutes of difference then it notify.
+# This script will check hwclock of the node.
+# Then compare hwclock with system time.
 
 # Nagios return codes
+OK=0
 WARNING=1
 CRITICAL=2
 UNKNOWN=3
-OK=0
 
 HWtime=$(sudo hwclock --debug | grep Hw | cut -d" " -f 8)
 SYStime=$(date +%s)
 let diff_time=$SYStime-$HWtime
 
-if [ $diff_time -gt 900 ] ; then
-   echo "Hardware clock is more than $diff_time seconds past now"
+if [ $diff_time -gt 300 ] ; then
+   echo "CRITICAL: Hardware clock is more than $diff_time seconds past now"
+   exit $CRITICAL
+
+elif [ $diff_time -lt 300 ] && [ $diff_time -gt 1 ] ; then
+   echo "WARNING: Hardware clock is more than $diff_time seconds past now"
    exit $WARNING
 
-elif [ $diff_time -lt 10 ] ; then
-   echo "Hardware clock is $diff_time secound past than system time"
+elif [ $diff_time -eq 0 ] ; then
+   echo "OK: Hardware clock is synced with system time"
    exit $OK
+
+else 
+   echo "Something Unknown happened here with hardware clock"
+   exit $UNKNOWN
 fi
