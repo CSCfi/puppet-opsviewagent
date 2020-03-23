240ccc6b0c43f0d6096e02c7721a474b6a559b98
Oscar Kraemer <oscar.kraemer@csc.fi>
2020-03-23 16:50:58 +0200

    Add non-admin nagios user support for cPouta
    CCCP-3079

--- files/nrpe/check_object.py
+++ files/nrpe/check_object.py
@@ -22,13 +22,9 @@ import json
 import urllib
 
 # Imports for API availability checks
-from swiftclient import client as swiftclient
+import swiftclient
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-from keystoneauth1 import loading
-from keystoneauth1 import session
-from keystoneauth1 import identity
-from keystoneclient.v3 import client as keystoneclientv3
 
 LOCAL_DEBUG           = False
 NAGIOS_STATE_OK       = 0
@@ -36,7 +32,7 @@ NAGIOS_STATE_WARNING  = 1
 NAGIOS_STATE_CRITICAL = 2
 NAGIOS_STATE_UNKNOWN  = 3
 
-DEFAULT_BUCKET_NAME = "nagiostestbucket4"
+DEFAULT_BUCKET_NAME = "nagiostestbucket4667"
 DEFAULT_DOMAIN_NAME = "default"
 
 USE_SECONDS              = True
@@ -49,65 +45,33 @@ class OSCredentials(object):
   and provide the credentials.
   '''
   cred = dict()
-  keystone_cred = dict()
   keystone_v3_cred = dict()
 
   def __init__(self, options):
     self.environment_credentials()
     self.options_credentials(options)
-    self.credentials_available()
 
   def environment_credentials(self):
     try:
-      self.cred['auth_url']   = os.environ['OS_AUTH_URL']
-      self.cred['username']   = os.environ['OS_USERNAME']
-      self.cred['api_key']    = os.environ['OS_PASSWORD']
-      self.cred['project_id'] = os.environ['OS_TENANT_NAME']
-      # Keystone only entries
-      self.keystone_cred['auth_url']    = os.environ['OS_AUTH_URL']
-      self.keystone_cred['username']    = os.environ['OS_USERNAME']
-      self.keystone_cred['password']    = os.environ['OS_PASSWORD']
-      self.keystone_cred['tenant_name'] = os.environ['OS_TENANT_NAME']
       # Keystone v3 only entries
       self.keystone_v3_cred['auth_url']    = os.environ['OS_AUTH_URL']
       self.keystone_v3_cred['user_id']    = os.environ['OS_USERNAME']
       self.keystone_v3_cred['password']    = os.environ['OS_PASSWORD']
       self.keystone_v3_cred['project_id'] = os.environ['OS_TENANT_NAME']
+      self.keystone_v3_cred['user_domain_name'] = os.environ['OS_USER_DOMAIN_NAME']
+      self.keystone_v3_cred['project_domain_name'] = os.environ['OS_PROJECT_DOMAIN_NAME']
     except KeyError:
       pass
 
   def options_credentials(self, options):
-    if options.auth_url: self.cred['auth_url']   = options.auth_url
-    if options.username: self.cred['username']   = options.username
-    if options.password: self.cred['api_key']    = options.password
-    if options.tenant  : self.cred['project_id'] = options.tenant
-    # Keystone only entries
-    if options.auth_url: self.keystone_cred['auth_url']    = options.auth_url
-    if options.username: self.keystone_cred['username']    = options.username
-    if options.password: self.keystone_cred['password']    = options.password
-    if options.tenant  : self.keystone_cred['tenant_name'] = options.tenant
-    # Keystone v3 only entries
+    options.user_domain_name = 'Default'
     if options.auth_url: self.keystone_v3_cred['auth_url']    = options.auth_url
-    if options.username: self.keystone_v3_cred['user_id']    = options.username
+    if options.username: self.keystone_v3_cred['username']    = options.username
     if options.password: self.keystone_v3_cred['password']    = options.password
-    if options.tenant  : self.keystone_v3_cred['project_id'] = options.tenant
+    if options.tenant  : self.keystone_v3_cred['project_name'] = options.tenant
+    if options.user_domain_name: self.keystone_v3_cred['user_domain_name'] = options.user_domain_name
+    if options.user_domain_name: self.keystone_v3_cred['project_domain_name'] = options.user_domain_name
 
-  def credentials_available(self):
-    for key in ['auth_url', 'username', 'api_key', 'project_id']:
-      if not key in self.cred:
-        raise CredentialsMissingException(key=key)
-    for key in ['auth_url', 'username', 'password', 'tenant_name']:
-      if not key in self.keystone_cred:
-        raise CredentialsMissingException(key=key)
-    for key in ['auth_url', 'user_id', 'password', 'project_id']:
-      if not key in self.keystone_v3_cred:
-        raise CredentialsMissingException(key=key)
-
-  def provide(self):
-    return self.cred
-
-  def provide_keystone(self):
-    return self.keystone_cred
 
   def provide_keystone_v3(self):
     return self.keystone_v3_cred
@@ -115,35 +79,22 @@ class OSCredentials(object):
 class OSSwiftAvailability():
   '''
   Check Swift API call length by listing containers with get_accounts
-  Only works with Keystone Auth URL v2.0
   '''
   options = dict()
 
   def __init__(self, options):
-    creds = OSCredentials(options).provide_keystone()
-    loader = loading.get_plugin_loader('password')
-    auth = identity.V2Password(**creds)
-    sessionx = session.Session(auth=auth)
-    #self.swift = swiftclient.Connection()
-    #TODO: Can one use OSCredentials??
-
-    _authurl = creds['auth_url']
-    _auth_version = 2
-    _user = creds['username']
-    _key = creds['password']
-    _os_options = {
-        'user_domain_name': "Default",
-        'project_domain_name': "Default",
-        'project_name': creds['tenant_name']
+    creds = OSCredentials(options).provide_keystone_v3()
+    os_options = {
+        'user_domain_name': creds['user_domain_name'],
+        'project_domain_name': creds['project_domain_name'],
+        'project_name': creds['project_name']
     }
-
-    self.conn = swiftclient.Connection(
-        authurl =_authurl,
-        user =_user,
-        key =_key,
-        os_options =_os_options,
-        auth_version =_auth_version
-    )
+    self.conn = swiftclient.client.Connection(
+                authurl = creds['auth_url'],
+                user = creds['username'],
+                key = creds['password'],
+                auth_version = '3',
+                os_options = os_options )
 
   def get_swift_buckets(self):
     resp_headers, containers = self.conn.get_account()
