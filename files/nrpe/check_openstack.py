6a4a35b751e3a14a204fb0c9702fb090d585cbd4
Oscar Kraemer <oscar.kraemer@csc.fi>
2020-03-24 16:36:14 +0200

    Update keystone test to not require admin user

--- files/nrpe/check_openstack.py
+++ files/nrpe/check_openstack.py
@@ -951,9 +951,10 @@ class OSKeystoneAvailability():
 
   def __init__(self, options):
     self.keystone = keystoneclientv3.Client(session=keystone_session_v3(options))
+    self.creds = OSCredentials(options).provide_keystone_v3()
 
   def get_keystone(self):
-    vols = self.keystone.projects.list()
+    user = self.keystone.users.get(self.creds['username'])
 
   def execute(self):
     results = dict()
