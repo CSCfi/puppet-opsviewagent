bbdfbef1f8e6ef31cc4447f0e112201d36c9c7b8
Matteo Pozza <matteo.pozza@csc.fi>
2021-12-31 14:07:01 +0200

    Simplify check_fernet_keys.py
    subcommand can be a bit dangerous and the .run call is not supported in
    python 2.7. This commit changes the check so that the subcommand module
    is not needed any longer.

--- files/nrpe/check_fernet_keys.py
+++ files/nrpe/check_fernet_keys.py
@@ -2,16 +2,12 @@
 
 import argparse
 import os.path
-import subprocess
 import sys
 
 NAGIOS_STATE_OK             = 0
 NAGIOS_STATE_WARNING        = 1
 NAGIOS_STATE_CRITICAL       = 2
 
-
-def run_shell_command(command):
-    return subprocess.run([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='utf-8').stdout
 
 def main():
     parser = argparse.ArgumentParser(description='Check the log file of the key generation process')
@@ -26,15 +22,19 @@ def main():
         print("CRITICAL - File " + args.log_file + " is empty")
         sys.exit(NAGIOS_STATE_CRITICAL)
     # file exists and is not empty
-    # take the last line
-    status_msg = run_shell_command("tail -n 1 " + args.log_file)
-    print(status_msg)
-    if status_msg.contains("OK"):
-        sys.exit(NAGIOS_STATE_OK)
-    elif status_msg.contains("WARNING"):
-        sys.exit(NAGIOS_STATE_WARNING)
-    # in all other cases, return critical state
-    sys.exit(NAGIOS_STATE_CRITICAL)
+    with open(args.log_file, "r") as log_file:
+        # take the last line
+        for line in log_file:
+          pass
+        # line contains the last line of the log file
+        status_msg = line
+        print(status_msg)
+        if status_msg.contains("OK"):
+            sys.exit(NAGIOS_STATE_OK)
+        elif status_msg.contains("WARNING"):
+            sys.exit(NAGIOS_STATE_WARNING)
+        # in all other cases, return critical state
+        sys.exit(NAGIOS_STATE_CRITICAL)
 
 if __name__ == "__main__":
     main()
