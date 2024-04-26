e46a303a0a356d87cc6c69d03866b446cef390e2
Kalle Happonen <kalle.happonen@csc.fi>
2024-04-26 16:26:13 +0300

    Fixed multiattach volume check
    Limited the multiattach volume check to only alert on non-multiattach
    volumes

--- files/nrpe/find_multiattached_volumes.sql
+++ files/nrpe/find_multiattached_volumes.sql
@@ -1,5 +1,4 @@
-# Prints instances from nova.block_device_mappings table that have multiple mappings
-# This is only valid while multi-attach is not supported in the platform.
+# Prints non-multiattach instances from nova.block_device_mappings table that have multiple mappings
 SELECT my_uuids
 FROM
 (
@@ -10,7 +9,8 @@ FROM
     nova.block_device_mapping
   WHERE
     deleted_at IS NULL AND
-    source_type="volume"
+    source_type="volume" AND
+    JSON_EXTRACT(connection_info,'$.multiattach') IS not True
   GROUP BY
     volume_id
   HAVING
