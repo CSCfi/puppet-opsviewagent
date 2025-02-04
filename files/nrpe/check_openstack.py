32cbe177e6f99a638cd0ebfef17e8f45e237ea03
Jukka-Pekka Jaatinen <jukka-pekka.jaatinen@csc.fi>
2025-02-04 16:19:18 +0200

    improve

--- files/nrpe/check_openstack.py
+++ files/nrpe/check_openstack.py
@@ -715,13 +715,12 @@ class OSL3Agent():
                                                                           agent['admin_state_up'],
                                                                           agent['alive'],
                                                                           agent['host']))
-
         elif agent['alive'] == True and agent['admin_state_up'] == False:
           if (
-             not (agent['configurations']['ex_gw_ports'] == 0) or
-             not (agent['configurations']['floating_ips'] == 0) or
-             not (agent['configurations']['interfaces'] == 0) or
-             not (agent['configurations']['routers'] == 0))
+               not (agent['configurations']['ex_gw_ports'] == 0) or
+               not (agent['configurations']['floating_ips'] == 0) or
+               not (agent['configurations']['interfaces'] == 0) or
+               not (agent['configurations']['routers'] == 0)
              ):
             CRITICAL = 1
           else:
@@ -731,7 +730,6 @@ class OSL3Agent():
                                                                             agent['admin_state_up'],
                                                                             agent['alive'],
                                                                             agent['host']))
-
         else:
           UNKNOWN = 1
           err_l3agents.append("%s (admin_state_up=%s, alive=%s) on %s" % (
